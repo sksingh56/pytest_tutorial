719c30f126d896096575809bc8c793aab9506b73
sanjay Singh <sksingh56@users.noreply.github.com>
2023-04-25 00:57:46 +0100

    Initial commit

---
+++ scripts/calculator.py
@@ -0,0 +1,7 @@
+def add_number(*args):
+    result = 0
+    for r in args:
+        result += r
+    return result
+
+    
---
+++ tests/calculator/test_calculator.py
@@ -0,0 +1,4 @@
+from scripts import calculator
+
+def test_add_number():
+    assert calculator.add_number(2,3,5,7) == 17
